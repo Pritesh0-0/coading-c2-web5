9e9a60f3c2cb6988991512be57bb3957f387fef3
Pritesh0-0 <100704776+Pritesh0-0@users.noreply.github.com>
2022-08-22 22:22:10 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import React from "react";
+
+export default function App() {
+  const [count,setCount] = React.useState(0);
+  fetch(`https://slides-app-220822.herokuapp.com/slides`)
+    .then((res) => res.json())
+    .then((res) =>  localStorage.setItem("item",JSON.stringify(res[count])))
+   
+    const data = JSON.parse(localStorage.getItem('item'));
+  
+return (
+  <div className="App">
+    <h1 data-cy="header">Slides</h1>
+    <div className="slide-container" data-cy="slide">
+      <h3 data-cy="title">{data.title}</h3>
+      <p data-cy="description">{data.description}</p>
+    </div>
+    <button data-cy="prev" disabled={count === 0} onClick={() => setCount(count-1)}>Prev</button>
+    <button data-cy="next" disabled={count === 4} onClick={() => setCount(count+1)}>Next</button>
+  </div>
+)
+}
